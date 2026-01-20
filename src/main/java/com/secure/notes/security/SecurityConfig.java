2549f040ac23a0056b0a160746e9397aa09baea0
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2026-01-20 08:18:32 +0530

    feat: Avoid creation of session instead of ignoring in every request
    Reference chatGPT for below. Need to verify.
    By default, spring creates session(JSESSIONID) in httpBasic but is not used for authentication.
    Using Stateless, we avoid creation of session

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.secure.notes.security;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,6 +18,9 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         // http.formLogin(withDefaults());
+        http.sessionManagement(session -> 
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
         http.httpBasic(withDefaults());
         return http.build();
     }
